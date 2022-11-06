73c14de705db62376612526bb4a19d189d595261
rochaeterno <robertodutraemp@gmail.com>
2022-11-06 17:24:14 -0300

    [Feature NYX-16]-desenvolver-servico-de-busca

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -12,7 +12,7 @@ import { Link } from '@inertiajs/inertia-vue3';
         </Link>
     </div>
 
-    <div class="py-12">
+    <div class="py-12 h-screen" style="background-color: rgb(26,47,48, 0.8);">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <main>
