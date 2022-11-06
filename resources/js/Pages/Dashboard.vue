ec87716156201403d44017af828b80a6f59c20e4
rochaeterno <robertodutraemp@gmail.com>
2022-11-06 17:57:18 -0300

    [Bugfix NYX-24]-limpar-dashboard-para-pr

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,13 +1,6 @@
 <script setup>
-import { onMounted } from 'vue'
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
-import api from '@/Services/api'
-import { Head, usePage } from '@inertiajs/inertia-vue3'
-
-onMounted(async () => {
-    const response = await api.searchTitle("Jack Reacher", usePage().props.value.apiKey)
-    console.log(response.results)
-})
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
@@ -15,7 +8,7 @@ onMounted(async () => {
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <div class="py-12" style="background-color: rgb(26,47,48, 0.6);">
+        <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
