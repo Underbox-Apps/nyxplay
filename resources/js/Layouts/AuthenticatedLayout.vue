a39cfb0cbc76a42fa60aa3e33a6ef02c1b667e9c
rochaeterno <robertodutraemp@gmail.com>
2022-11-06 22:21:28 -0300

    [Feature WIP NYX-23]-desenvolver-header-bar-de-busca

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,15 +1,34 @@
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
+import SearchInput from '@/Components/SearchInput.vue';
 </script>
 
 <template>
-    <div class="flex flex-row justify-between">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Dashboard
-        </h2>
-        <Link :href="route('logout')" method="post" as="button">
+    <div class="flex flex-row justify-between px-5 py-2" style="background-color:rgb(26,47,48, 0.95);">
+        <div class="flex flex-row w-4/5">
+            <div class="hidden lg:flex flex-col place-content-center">
+                <div class="flex h-3/5 w-auto">
+                    <!-- logo -->
+                    <img class="flex object-cover w-auto h-full shadow rounded-full" src="assets/nyx-logo.jpg"
+                        alt="Nyx Tecnology">
+                </div>
+            </div>
+
+            <!-- Busca -->
+            <div class="flex flex-col place-content-center lg:mx-5 w-full">
+                <div class="flex flex-row justify-between">
+                    <SearchInput />
+                </div>
+            </div>
+        </div>
+
+        <!-- Logout Button -->
+        <div class="flex flex-col place-content-center">
+            <Link :href="route('logout')" method="post" as="button" class="inline-flex items-center text-white text-sm">
             Logout
-        </Link>
+            </Link>
+        </div>
+
     </div>
 
     <div class="py-12">
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->prefix('dashboard')->group(function () {
+    Route::get('/search', function () {return Inertia::render('Search');})->name('search');
+    Route::get('/',  function () {return Inertia::render('Dashboard');})->name('home');
+});
 
 require __DIR__.'/auth.php';
