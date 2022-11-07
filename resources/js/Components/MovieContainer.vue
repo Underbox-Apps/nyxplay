327976ba627ed6470263522040e63e17498ad5e9
rochaeterno <robertodutraemp@gmail.com>
2022-11-07 11:17:19 -0300

    [Feature WIP NYX-6]-rascunho-movie-container

---
+++ resources/js/Components/MovieContainer.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+<template>
+    <div class="flex flex-row h-full mx-2 my-2 w-full place-content-start shadow-md shadow-black">
+        <div class="flex flex-col h-full min-w-fit">
+            <!-- 'https://image.tmdb.org/t/p/original/' + result.poster_path -->
+            <img class="h-full w-full object-scale-down"
+                src="https://image.tmdb.org/t/p/original/uQBbjrLVsUibWxNDGA4Czzo8lwz.jpg">
+        </div>
+
+        <div class="flex flex-col grow w-full h-full place-self-start bg-black bg-opacity-50 pr-4 pl-5 py-3">
+            <!-- result.title -->
+            <div class="text-white text-xl font-bold pb-3">Jack Reacher</div>
+
+            <!-- result.overview -->
+            <div class="text-gray-300 overflow-y-auto text-sm text-justify">When a gunman takes five lives with six
+                shots, all evidence points
+                to the suspect in
+                custody. On interrogation, the suspect offers up a single note: \"Get Jack Reacher!\" So begins an
+                extraordinary chase for the truth, pitting Jack Reacher against an unexpected enemy, with a skill for
+                violence and a secret to keep When a gunman takes five lives with six shots, all evidence points to the.
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/Components/MovieShowcase.vue
+++ resources/js/Components/MovieShowcase.vue
@@ -1,13 +1,14 @@
 <script setup>
-import MovieShowcaseRow from '@/Components/MovieShowcaseRow.vue';
+import MovieContainer from '@/Components/MovieContainer.vue';
 </script>
 <template>
     <div class="relative flex flex-col items-start min-h-screen min-w-screen bg-scroll sm:pt-0"
         style="background-image: url('/assets/cinesala.webp'); background-size: cover; background-repeat: no-repeat; background-position: center;">
-        <div
-            class="relative flex flex-col items-start h-screen w-full bg-scroll sm:pt-0 bg-gray-500 bg-opacity-30 ">
-            <div class="my-4 w-full">
-                <MovieShowcaseRow :title="'Mais Assistidos'" />
+        <div class="relative flex flex-col items-start h-screen w-full bg-scroll sm:pt-0 bg-gray-500 bg-opacity-30 ">
+            <div class="flex flex-row my-4 w-full h-2/5 overflow-y-hidden">
+                <div class="h-full w-2/5 py-4">
+                    <MovieContainer />
+                </div>
             </div>
         </div>
     </div>
