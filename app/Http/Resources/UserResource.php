9e613fcd5c399ec3d454e716fc6d2522e697d078
rochaeterno <robertodutraemp@gmail.com>
2022-11-08 17:13:31 -0300

    [Feature NYX-36]-criar-resources-para-as-tabelas

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        try {
+            return [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ];
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => true,
+                "serve_error_message" => $e->getMessage()
+            ], 402);
+        }
+    }
+}
