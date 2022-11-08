821b226d287509680da4408fd0dd2c64d30a36cd
rochaeterno <robertodutraemp@gmail.com>
2022-11-08 17:13:31 -0300

    [Feature NYX-36]-criar-resources-para-as-tabelas

---
+++ app/Http/Resources/RentedMoviesCollection.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class RentedMoviesCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        try {
+            return [
+                "id" => $this->id,
+                "movie_id" => $this->movie_id,
+                "movie_name" => $this->movie_name,
+                "user_id" => $this->user_id,
+                "created_at" => $this->created_at,
+                "expiration_date" => $this->expiration_date,
+                "user" => new UserResource($this->user_id),
+            ];
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => true,
+                "serve_error_message" => $e->getMessage()
+            ], 402);
+        }
+
+    }
+}
