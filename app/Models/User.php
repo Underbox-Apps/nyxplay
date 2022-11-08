cb51ed949df2f4d46362e930327f37f8393a1bc2
rochaeterno <robertodutraemp@gmail.com>
2022-11-08 16:36:01 -0300

    [Feature NYX-12]-criar-migration-e-model-de-aluguel

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function movies_rented()
+    {
+        return $this->hasMany(MoviesRented::class);
+    }
 }
