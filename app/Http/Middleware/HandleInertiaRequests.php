95bf620e01b22af597d1a50de11d00738c870f63
rochaeterno <robertodutraemp@gmail.com>
2022-11-09 05:38:53 -0300

    [Feature NYX-40]-ordenar-titulos-alugados-pordata-de-criacao-desc

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,7 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request)
     {
-        $movies = MoviesRented::all();
+        $movies = MoviesRented::where('expiration_date', '>=', now())->orderBy('created_at', 'desc')->get();
         return array_merge(parent::share($request), [
             'auth' => [
                 'user' => $request->user(),
