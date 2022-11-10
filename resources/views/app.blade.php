fdb8d3e13786c20e178dda600fd22744b1e55a69
rochaeterno <robertodutraemp@gmail.com>
2022-11-09 21:58:35 -0300

    [Feature NYX-48]-adicionar-favicon-e-personalizar-o-titulo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="shortcut icon" href="{{ asset('assets/nyx-logo.jpg') }}">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
